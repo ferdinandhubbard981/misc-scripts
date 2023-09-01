0792f57affd2d47ea06cecd6e8cbcd7567fd2cf0
ferdinandhubbard981 <ferdinandhubbar@gmail.com>
2023-09-01 15:47:53 +0100

    update

---
+++ gptGUI.sh
@@ -0,0 +1,4 @@
+current_dir = $PWD
+cd ~/ChuanhuChatGPT
+python3 ChuanhuChatbot.py
+cd current_dir
--- ssh/gcloud-ssh-reverse.sh
+++ ssh/gcloud-ssh-reverse.sh
@@ -1,1 +1,4 @@
+gcloud compute config-ssh --remove
+gcloud compute config-ssh
+sed -i 's/ubuntu-dev-vm.europe-west1-b.third-harbor-389121/googledev/g' ~/.ssh/config
 gcloud compute ssh --zone "europe-west1-b" "ubuntu-dev-vm" --project "third-harbor-389121" -- -R 2000:localhost:2000
